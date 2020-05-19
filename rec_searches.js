c59aa2f54bbb46bf94c63f9c6b7fd84fe9780a80
Jorge25G <jgonzreal62@gmail.com>
2020-05-18 21:19:49 -0500

    trying the search process
    implementing the js code in order to obtain the data and manipulate it so the user can make a search and the recent searches will be shown

---
+++ rec_searches.js
@@ -0,0 +1,23 @@
+/* Document for recent searches */
+const jobsCarritos4Sale = [
+    //differents works here
+    {
+        "job": "CEO",
+        "bussines": "Carritos4Sale"
+    },
+    {
+        "job": "Analista de Datos",
+        "business": "Carritos4Sale"
+    },
+    {
+        "job": "Vendedor",
+        "business": "Carritos4Sale"
+    }
+]
+
+const searchedJob = document.querySelector("#s_job")
+const button = document.querySelector("#btn")
+
+const filter = () => {
+    console.log(jobsCarritos4Sale)
+}
